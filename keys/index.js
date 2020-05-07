0c7c862ee3f0477024fd5fff2c194725d71d1787
ShadCheb <vas.box@mail.ru>
2020-05-07 16:13:09 +0400

    'keys_bd_change'

--- keys/index.js
+++ keys/index.js
@@ -5,5 +5,8 @@ module.exports = {
   EMAIL_PASSWORD: 'rapira1507',
   BASE_URL: 'http://localhost:3000',
   NAME_SITE: 'Газовед',
-  TOKEN_VK: '2b69ab252f4868d74de6503b4ec9eec5cd13d961e2b08ffcb94dd98bc340e063a65df5068f4136b470966'
+  TOKEN_VK: '2b69ab252f4868d74de6503b4ec9eec5cd13d961e2b08ffcb94dd98bc340e063a65df5068f4136b470966',
+  DB_NAME: 'gbo_db',
+  USER_NAME: 'g_admin',
+  PASSWORD: 'yd4D59sp!W'
 }
--- utils/database.js
+++ utils/database.js
@@ -1,7 +1,5 @@
 const Sequelize = require('sequelize');
-const DB_NAME = 'gbo_db';
-const USER_NAME = 'g_admin';
-const PASSWORD = 'yd4D59sp!W';
+const {DB_NAME, USER_NAME, PASSWORD} = require('../keys/index');
 
 const sequelize = new Sequelize(DB_NAME, USER_NAME, PASSWORD, {
   host: 'localhost',
