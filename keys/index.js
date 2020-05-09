b861cdb2995de11ac34658ca7ff4a0772c84d181
ShadCheb <vas.box@mail.ru>
2020-05-09 19:42:10 +0400

    'add_keys'

---
+++ keys/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  SESSION_SECRET: 'some secret value',
+  EMAIL_FROM: 't3.t3st@yandex.ru',
+  EMAIL_USER: 't3.t3st',
+  EMAIL_PASSWORD: 'rapira1507',
+  BASE_URL: 'http://localhost:3000',
+  NAME_SITE: 'Газовед',
+  TOKEN_VK: '2b69ab252f4868d74de6503b4ec9eec5cd13d961e2b08ffcb94dd98bc340e063a65df5068f4136b470966',
+  DB_NAME: 'gbo_db',
+  USER_NAME: 'g_admin',
+  PASSWORD: 'yd4D59sp!W'
+}
